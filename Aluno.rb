9795805434f85d6dd0c1445dda7793acffd5a740
Wilker <Wilker.rj@gmail.com>
2017-07-06 11:43:50 -0300

    Classe Aluno iniciada

---
+++ Aluno.rb
@@ -0,0 +1,17 @@
+class Aluno
+    attr_accessor :nome
+    attr_accessor :matricula
+    attr_accessor :telefone
+    attr_accessor :email #refatorar para incluir uma classe email
+    attr_accessor :uffmail #refatorar para incluir uma classe email
+    attr_accessor :status
+    
+    def initialize(nome, matricula, telefone, email, uffmail, status)
+        @nome = nome 
+        @matricula = matricula
+        @telefone = telefone
+        @email = email
+        @uffmail = uffmail
+        @status = status
+    end
+end

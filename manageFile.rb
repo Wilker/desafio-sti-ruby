426abb852733d7b407bce256357733c7ed888510
Wilker <Wilker.rj@gmail.com>
2017-07-10 11:21:46 -0300

    Adicionada função para carregar o arquivo para classes Aluno

--- manageFile.rb
+++ manageFile.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require_relative 'aluno'
 
 class ManageFile
     attr_accessor :path
@@ -6,13 +7,19 @@ class ManageFile
     def initialize(path)
         @path = path
     end
-    
-    def read()
+ 
+=begin
+    def read
         alunos = [] 
         CSV.foreach(path, headers: true){ 
         |row| alunos << row.to_hash
         }
         return alunos
     end
-    
+=end
+
+    # Função para carregar cada linha do arquivo para um objeto da classe aluno
+    def read
+    alunos = CSV.read(path).collect{ |row| Aluno.new *row }
+    end
 end    

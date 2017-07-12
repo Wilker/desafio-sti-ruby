d9b634a14d77c1878b0ce701e83cc906a1eb6750
Wilker <Wilker.rj@gmail.com>
2017-07-12 19:43:20 -0300

    Resolvido bug na leitura do valor passado e adicionado proteção contra opções de email inválidas]

--- menu.rb
+++ menu.rb
@@ -1,32 +1,35 @@
 module Menu
 
     def menuInicial
-    puts "Insira sua matricula:"
-    mat = gets.chomp
+        puts "Insira sua matricula:"
+        mat = gets.chomp
     end
-    
+
     def userInactive
-    puts "Somente alunos ativos podem criar uffmail"
+        puts "Somente alunos ativos podem criar uffmail"
     end
-   
+
     def uffMailExists
-    puts "Usuario ja possui uffmail"
+        puts "Usuario ja possui uffmail"
     end
-    
+
     def matNotExists
-    puts "Matricula invalida"
-    end    
-    
-    def optionsEmail(aluno)
-    puts "#{aluno.nome}, por favor escolha uma das opcoes abaixo para o seu UFFMail"
-    options = aluno.email.generateEmail(aluno.nome)
-    puts options
-    option = gets.chomp
-    selected = options[option.to_i+1]
+        puts "Matricula invalida"
     end
-    
+
+    def optionsEmail(aluno)
+        puts "#{aluno.nome}, por favor escolha uma das opcoes abaixo para o seu UFFMail"
+        options = aluno.email.generateEmail(aluno.nome)
+        option = ""
+        loop do
+            puts options
+            option = gets.chomp.to_i
+            break if option > 0 && option <= options.size
+        end
+        selected = options[option.to_i-1]
+    end
+
     def emailCreated(aluno)
-    puts "A criacao de seu e-mail (#{aluno.uffmail.email}) sera feita nos proximos minutos.
-Um SMS foi enviado para #{aluno.telefone} com a sua senha de acesso."
+        puts "A criacao de seu e-mail (#{aluno.uffmail.email}) sera feita nos proximos minutos.\nUm SMS foi enviado para #{aluno.telefone} com a sua senha de acesso."
     end
 end
